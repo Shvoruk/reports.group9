62b04164654f9f7636499626a3bf961f4cad8a25
aleks <aleksey.shvoruk@gmail.com>
2024-10-07 21:27:22 +0100

    add Controller/ workflow test

---
+++ src/main/java/com/coursework/reports/controller/ReportController.java
@@ -0,0 +1,15 @@
+package com.coursework.reports.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/")
+public class ReportController {
+
+    @GetMapping
+    public String hello(){
+        return "hello";
+    }
+}
